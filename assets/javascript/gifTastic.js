484740e5a0dafec2255f7c0d8305c6233be64d30
stacieknisley <stacieknisley@juno.com>
2018-06-04 23:42:49 -0400

    upated files

--- assets/javascript/gifTastic.js
+++ assets/javascript/gifTastic.js
@@ -1,18 +1,18 @@
 // set document so everything is down loaded before game begins.
-$(document ).ready(function() {
-
+$(document).ready(function() {
+  
     // click button and search for api gify images from queryURl, 
     // variable is x and the jquery $ is searching for data from the variable queryURL
     $("button").on("click", function () {
         var x = $(this).data("search");
   
         // gif query asking for data from api gify using api.giphy website and my key with a limit of 10 gif images.
-        var queryURl = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=8Jaon0lnqL45mbSJUDqgV8WgFfy2DQ8E&limit=10"
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=fq0AX58MelCuXjluMawLbu45ZSBrTruc&limit=10"
         console.log(queryURL);
   
         // <!--make ajax request  call to the ajax URL and use the GET method-->
-        $.ajax({ url: queryURL, method: "GET" })
-          .done(function (response) {
+        $.ajax({ query: queryURL,method:"GET"})
+          .then(function (response) {
             console.log(response);
   
             for (var i = 0; i < response.data.length; i++) {
@@ -21,7 +21,7 @@ $(document ).ready(function() {
                 }  
 
     
-       
+              });
                 });
 
 // --------------jquery code to allow users to add and submit more instruments -------
@@ -30,35 +30,43 @@ $(document ).ready(function() {
 
 // When information or an instrument is entered into the  "form-control" button place holder the following function
 // takes place, the variable "htmlString" or information is entered as text.
-$("form-control").click(function(){
-    var htmlString = $(this) .html();
-    $( this ).text (htmlString);
+$("#submitButton").click(function(){
+    var htmlString = $(".form-control").val().trim();
+    console.log(htmlString);
+    
+    // then a button is created from the text that was entered
+    var button = $("<button>").text(htmlString).attr("data-search",htmlString);
+    $(".container").append(button);
+    
 });
+
 
 
 // create an on click event when the "Submit" button is clicked.
 // the "Submit" button has a class label of "btn-primary" from bootstrap css.
 
-$("btn btn-primary").on("click",function(){
+// $("btn btn-primary").on("click",function(){
 
-    // variables defined for the onclick event. Used "y" because "x" and "i" were used above
-    // y is the data/instrument entered into the form-control button
-    var y = $(this).data("form-control");
-    // queryURL, asking for the giphy images from the api giphy web site
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=8Jaon0lnqL45mbSJUDqgV8WgFfy2DQ8E&limit=10";
+//     // variables defined for the onclick event. Used "y" because "x" and "i" were used above
+//     // y is the data/instrument entered into the form-control button
+//     var y = $(this).data(".form-control");
+
+
+//     // queryURL, asking for the giphy images from the api giphy web site
+//     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=8Jaon0lnqL45mbSJUDqgV8WgFfy2DQ8E&limit=10";
     
-    // <!--make ajax request call to the ajax URL and use the GET method for the additional instruments added by the user-->
-    $.ajax({ url: queryURL, method: "GET" })
-          .done(function (response) {
-            // console.log(response);
-  
-            for (var y = 0; y < response.data.length; y++) {
-              $("#GIFArea").prepend("<p>Rating: " + response.data[y].rating + " </p>");
-              $("#GIFArea").prepend("<img src='" + response.data[y].images.downsized.url + "'>");
+//     // <!--make ajax request call to the ajax URL and use the GET method for the additional instruments added by the user-->
+//     $.ajax({ url: queryURL, method: "GET" })
+//           .done(function (response) {
+//             // console.log(response);
+//             // this will add a rating and downsize the giphys selected from the api ajax call
+//             for (var y = 0; y < response.data.length; y++) {
+//               $("#GIFArea").prepend("<p>Rating: " + response.data[y].rating + " </p>");
+//               $("#GIFArea").prepend("<img src='" + response.data[y].images.downsized.url + "'>");
                 
-            }  
+//             }  
 
-});
+// });
 
 
  // -------------jquery code to start and stop action of gify images
@@ -80,4 +88,5 @@ $("btn btn-primary").on("click",function(){
 
 
 // --------having problems determining how to close out the  line below error in console.log ???
-    }
+});
+});
